8712a1e1a852e6af12456cde44d33276b2300c93
Poojitha Lagidi <160959553+poojitha-lagidi@users.noreply.github.com>
2024-03-30 22:17:48 +0100

    D FF

--- d_ff.v
+++ d_ff.v
@@ -1,10 +1,10 @@
 module d_ff(
 	input reset,
-    input clk,
-    input d,
+	input clk,
+	input d,
 	output reg q,
-    output q_not
-    );
+	output q_not
+);
 	
 	always @(posedge clk or negedge reset) begin
 	    if (!reset)
@@ -21,13 +21,13 @@ endmodule
 `timescale 1us/1ns
 module d_ff_tb();
 
-    reg d;
+	reg d;
 	reg clk = 0;
 	reg reset;
 	wire q;
-    wire q_not;
+	wire q_not;
 	reg [1:0] delay;
-    integer i;
+	integer i;
 	
 	// Instantiate the DUT
 	d_ff DFF0( .reset(reset), .clk(clk), .d(d), .q(q), .q_not(q_not));
@@ -37,19 +37,19 @@ module d_ff_tb();
 	    #0.5 clk = ~clk;
 	end
 	
-    // Create stimulus	  
-    initial begin
-        reset = 0; d = 0;
+    	// Create stimulus	  
+   	initial begin
+        	reset = 0; d = 0;
 		#1;	reset = 1;
-	    for (i=0; i<5; i=i+1) begin
+	    	for (i=0; i<5; i=i+1) begin
 		   delay = $urandom_range(1,3);
 		   #(delay) d = ~d; 
 		end	
-        #(0.2); reset = 0; // reset the FF again		
+        	#(0.2); reset = 0; // reset the FF again		
 	end
 	
-    // This will stop the simulator when the time expires
-    initial begin
-        #10 $stop;
-    end  
+    	// This will stop the simulator when the time expires
+    	initial begin
+        	#10 $stop;
+    	end  
 endmodule
