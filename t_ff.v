1a3297ecf130d5e0e38ea306811ab96dfd0d3e3a
Poojitha Lagidi <160959553+poojitha-lagidi@users.noreply.github.com>
2024-03-30 22:22:27 +0100

    T  FF

--- t_ff.v
+++ t_ff.v
@@ -1,8 +1,8 @@
 module t_ff(
-	input reset,
+    input reset,
     input clk,
     input t,
-	output reg q,
+    output reg q,
     output q_not
     );
 	
@@ -21,13 +21,13 @@ endmodule
 `timescale 1us/1ns
 module t_ff_tb();
 
-    reg t;
+   	reg t;
 	reg clk = 0;
 	reg reset;
 	wire q;
-    wire q_not;
+   	wire q_not;
 	reg [1:0] delay;
-    integer i;
+    	integer i;
 	
 	// Instantiate the DUT
 	t_ff TFF0( .reset(reset), .clk(clk), .t(t), .q(q), .q_not(q_not));
@@ -37,19 +37,19 @@ module t_ff_tb();
 	    #0.5 clk = ~clk;
 	end
 	
-    // Create stimulus	  
-    initial begin
-        reset = 0; t = 0;
+   	// Create stimulus	  
+   	initial begin
+        	reset = 0; t = 0;
 		#1;	reset = 1;
-	    for (i=0; i<5; i=i+1) begin
+	    	for (i=0; i<5; i=i+1) begin
 		   delay = $urandom_range(1,3);
 		   #(delay) t = ~t; // toggle the FF at random times
 		end	
-        #(0.2); reset = 0; // reset the FF again		
+       	 	#(0.2); reset = 0; // reset the FF again		
 	end
 	
-    // This will stop the simulator when the time expires
-    initial begin
-        #10 $stop;
-    end  
+    	// This will stop the simulator when the time expires
+    	initial begin
+        	#10 $stop;
+    	end  
 endmodule
